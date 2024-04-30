2f6f6feaf6370b40fd1c35fea173cd337d46f5d2
Russell Wallace <russell.wallace@gmail.com>
2024-04-30 16:22:10 +0100

    main

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 
 internal class Program {
 	static void Main(string[] args) {
-		Console.WriteLine("Hello, World!");
+		var filename = args[0];
+		Console.WriteLine(MimeUtility.GetMimeMapping(filename));
 	}
 }
